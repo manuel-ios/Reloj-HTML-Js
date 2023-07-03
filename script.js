54c8687fe682352f287c891e3441076e81322443
manuel-ios <manuel.ios.catalan@gmail.com>
2023-07-03 14:48:52 -0600

    Final JS

--- script.js
+++ script.js
@@ -9,10 +9,24 @@ function reloj(){
     min =  min <= 9 ? "0"+min: min;
     seg =  seg <= 9 ? "0"+ min : seg;
     let horaActual =  hr + ":" + min + ":" + seg 
-    console.log (horaActual)
-
+    let hrDiv  = document.createElement('div')
     setTimeout(()=>{
         reloj()
     },2000)
+
+    if(hr => 12){
+        // console.log("Buenas Tardes"+horaActual)
+        let hrDiv  = document.createElement('div')
+        hrDiv.innerHTML = `<h1><center>Buenas Tardes</center></h1>`
+        document.body.append(hrDiv)
+    
+    }else{
+        console.log(
+            "Buenos Días"+horaActual)
+    }
+    
+
+    
 }
 reloj()
+
